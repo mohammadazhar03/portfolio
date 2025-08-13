a01bf62828cb6e00f1a0ae1f18a4ed29bebfcac6
Mohammad Azhar <azharmohd2703@gmail.com>
2025-08-14 00:23:25 +0530

    update education

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import '../Styles//Education.css';
 
 const educationData = [
-  { degree: "B.E in Information Technology", school: "Lords Institute of Engineering Technology", year: "2019 - 2023" },
-  { degree: "Diploma in Mechanical Engineering", school: "Sri Sangameshwara Govt Polytechnic", year: "2016 - 2019" }
+  { degree: "B.E in Information Technology", school: "Lords Institute of Engineering Technology", year: "2021 - 2024" },
+  { degree: "Diploma in Mechanical Engineering", school: "Sri Sangameshwara Govt Polytechnic", year: "2018 - 2021" }
 ];
 
 const Education = () => {
